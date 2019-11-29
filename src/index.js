26b5835896d1c91b33c89c60d064fd34b16fd1c8
justjack555 <jjricci555@gmail.com>
2019-11-28 20:10:46 -0500

    Can hit the golang server

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import _ from 'underscore';
+
+class Opportunities extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      oppsLoaded: false,
+      opportunities: []
+    };
+  }
+
+  componentDidMount(){
+    if(this.state.oppsLoaded) return;
+
+    console.log("fetching opps...");
+    fetch('http://localhost:8080/', {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin': 'localhost',
+      }
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            oppsLoaded: true,
+            opportunities: result.opportunities
+          });
+        },
+        (errorResp) => {
+          console.log("Error retrieving opportunities: ", errorResp.err);
+        }
+      )
+  }
+
+  render() {
+    return (
+      <div className="opportunities">
+        <h1>Opportunities</h1>
+        { this.renderOpportunities() }
+      </div>
+    );
+  }
+
+  renderOpportunities = () => {
+    return (
+      <div className="opportunities-wrapper">
+        { _.each(this.state.opportunities, (opportunity) => {
+          return <div className="opportunity">{ opportunity } </div>
+        })}
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Opportunities />,
+  document.getElementById('root')
+);
